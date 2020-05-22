08b2579efef84807af5e599284f8aaba3a6bb184
Alejandro Alamar <alejaam@users.noreply.github.com>
2020-05-21 19:22:05 -0500

    Agregando routes y paginas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:form_validator/pages/home_page.dart';
+import 'package:form_validator/pages/login.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,12 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
       debugShowCheckedModeBanner: false,
-       initialRoute: 'login',
-       routes: {
-         'login':
-       },
+      title: 'Material App',
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => LoginPage(),
+        'home': (BuildContext context) => HomePage()
+      },
     );
   }
 }
