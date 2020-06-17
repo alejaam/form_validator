ebb8c83412259de026c582e1a029b766cfd0910d
Alejandro Alamar <alejaam@users.noreply.github.com>
2020-06-16 19:33:29 -0500

    Agregando bloc de productos

---
+++ lib/src/bloc/productos_bloc.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:rxdart/rxdart.dart';
+
+import 'package:form_validator/src/models/producto_model.dart';
+import 'package:form_validator/src/providers/productos_provider.dart';
+
+class ProductosBloc {
+  final _productosController = new BehaviorSubject<List<ProductoModel>>();
+  final _cargandoController = new BehaviorSubject<bool>();
+  final _productosProvider = new ProductosProvider();
+
+  Stream<List<ProductoModel>> get productosStream =>
+      _productosController.stream;
+  Stream<bool> get cargando => _cargandoController.stream;
+
+  void cargarProductos() async {
+    final productos = await _productosProvider.cargarProductos();
+    _productosController.sink.add(productos);
+  }
+
+  void agregarProductos(ProductoModel producto) async {
+    _cargandoController.sink.add(true);
+    await _productosProvider.crearProducto(producto);
+    _cargandoController.sink.add(true);
+  }
+
+  Future<String> subirFoto(File foto) async {
+    _cargandoController.sink.add(true);
+    final fotoUrl = await _productosProvider.subirImagen(foto);
+    _cargandoController.sink.add(true);
+
+    return fotoUrl;
+  }
+
+  void editarPoduct(ProductoModel producto) async {
+    _cargandoController.sink.add(true);
+    await _productosProvider.editarProducto(producto);
+    _cargandoController.sink.add(false);
+  }
+
+  void borrarPoducto(String id) async {
+    await _productosProvider.borrarProducto(id);
+  }
+
+  dispose() {
+    _productosController?.close();
+    _cargandoController?.close();
+  }
+}
